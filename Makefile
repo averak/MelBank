efa00fc0b5ec9d48b248533be19711063104611f
Lab-info <abe12@mccc.jp>
2019-09-14 22:28:50 +0900

    add stft

--- Makefile
+++ Makefile
@@ -7,4 +7,3 @@ record.real: ./lib/scripts/detection.py
 # 一度だけ音声録音
 record.once: ./lib/scripts/recording.py
 	@python ./lib/scripts/recording.py
-
---
+++ test/to_spec.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+import scipy.io.wavfile as wf
+from scipy import signal
+
+def stft(wav, to_log=True):
+    ## -----*----- STFT（短時間フーリエ変換） -----*----- ##
+    _, _, spec = signal.stft(wav, fs=8000, nperseg=255)
+    if to_log:
+        spec = 10 * np.log(np.abs(spec))
+    return spec
+
+
+wav = wf.read('./lib/assets/format.wav')[1]
+wav = wf.read('./tmp/source.wav')[1]
+spec = stft(wav)
