269bf4ba47f440431c2871ff0fe2422d38ccd3da
Lab-info <abe12@mccc.jp>
2019-09-16 18:25:01 +0900

    話者の音声を格納するディレクトリの作成処理追加

--- make_teacher.py
+++ make_teacher.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from lib.scripts.recording import Recording
 import time
+import os
 
+# ===== 新規話者の登録 ===============
+dir = './tmp/speaker'
+# 標準入力で話者名を指定
+print('新規話者： ', end='')
+name = input()
+# ディレクトリ作成
+os.makedirs('{0}/{1}'.format(dir, name), exist_ok=True)
 
-# ========== 録音 ====================
+exit (0)
+
 record = Recording()
+for i in range(10):
+    record.file = '{0}/{1}/{2}.wav'.format(dir, name, i)
+
 time.sleep(1)
 print('start')
 record.record_start.set()
