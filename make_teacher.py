9aa28be54529e6ce80e15f997566b17594356f48
Lab-info <abe12@mccc.jp>
2019-09-16 18:56:19 +0900

    教師データの作成スクリプトの完成

--- make_teacher.py
+++ make_teacher.py
@@ -5,21 +5,24 @@ import os
 
 # ===== 新規話者の登録 ===============
 dir = './tmp/speaker'
+
 # 標準入力で話者名を指定
 print('新規話者： ', end='')
 name = input()
 # ディレクトリ作成
 os.makedirs('{0}/{1}'.format(dir, name), exist_ok=True)
 
-exit (0)
-
 record = Recording()
-for i in range(10):
-    record.file = '{0}/{1}/{2}.wav'.format(dir, name, i)
+os.system('clear')
 
-time.sleep(1)
-print('start')
-record.record_start.set()
-time.sleep(0.3)
-record.record_start.clear()
+for i in range(10):
+    time.sleep(2)
+    record.file = '{0}/{1}/{2}.wav'.format(dir, name, i + 1)
+    print("===== {0} START ===============".format(i + 1))
+    time.sleep(0.5)
+    record.record_start.set()
+    time.sleep(1.0)
+    record.record_start.clear()
+    print("===== END ===============\n")
+
 record.is_exit = True
