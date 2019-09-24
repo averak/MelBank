f0fb429bf6a6c7f741a81828e75abc605228f3c2
Lab-info <abe12@mccc.jp>
2019-09-24 10:55:16 +0900

    テストスクリプトの変更

--- test/mixed.py
+++ test/mixed.py
@@ -2,7 +2,7 @@
 import scipy.io.wavfile as wf
 import glob
 print(glob.glob('./tmp/teach/*'))
-voice1 = wf.read(glob.glob('./tmp/teach/阿部/*.wav')[0])[1]
-voice2 = wf.read(glob.glob('./tmp/teach/お天気お姉さん/*.wav')[0])[1]
+voice1 = wf.read(glob.glob('./tmp/teach/target/*.wav')[0])[1]
+voice2 = wf.read(glob.glob('./tmp/teach/other/*.wav')[0])[1]
 mixed = voice1 + voice2
 wf.write('./tmp/mixed.wav', 8000, mixed)
