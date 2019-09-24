8c3e344d1da05136ce1d0a83f1321079c7370949
Lab-info <abe12@mccc.jp>
2019-09-24 12:36:40 +0900

    リアルタイムでの音声入出力を行うテストスクリプト作成

--- test/plot_wav.py
+++ test/plot_wav.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import glob
 
 
-fs, wave1 = wf.read('./tmp/mixed.wav')
-fs, wave1 = wf.read(glob.glob('./tmp/teach/800Hz/*.wav')[0])
+fs, wave1 = wf.read('./tmp/source.wav')
 fs, wave2 = wf.read('./tmp/separate.wav')
+
 plt.plot(wave1)
 plt.plot(wave2)
 plt.show()
