17c822f0b0cf93f682bc003e6d445abe225f5eda
AtLab-jp <abe12@mccc.jp>
2019-11-07 14:58:12 +0900

    コード整形

--- separator.py
+++ separator.py
@@ -14,7 +14,7 @@ class Separator(object):
         ## -----*----- コンストラクタ -----*-----##
         # ファイルパス
         self.format_path = './config/format.wav'
-        self.model_path = './model/model.hdf5'
+        self.model_path  = './model/model.hdf5'
         self.output_path = './tmp/separate.wav'
 
         # サンプリングレート
